9bc92dc25313c74c8c5def7a36a90902621a3279
averma2018 <averma2018@gmail.com>
2023-10-16 23:16:06 -0400

    modified:   "pages/3_\360\237\244\223_Ask_Ellie.py" modified:   "pages/3_\360\237\244\223_Ask_Ellie.py"

--- pages/3_🤓_Ask_Ellie.py
+++ pages/3_🤓_Ask_Ellie.py
@@ -1,69 +1,36 @@
-#################################################################################
-# Chat2VIS 
-# https://chat2vis.streamlit.app/
-# Paula Maddigan
-#################################################################################
-
 import pandas as pd
 import openai
 import streamlit as st
-#import streamlit_nested_layout
 from classes import get_primer,format_question,run_request
 import warnings
 warnings.filterwarnings("ignore")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_icon="chat2vis.png",layout="wide",page_title="Chat2VIS")
 
+# ... [skipping the UI design code and model selection for brevity] ...
 
-st.markdown("<h1 style='text-align: center; font-weight:bold; font-family:comic sans ms; padding-top: 0rem;'> \
-            Chat2VIS</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;padding-top: 0rem;'>Creating Visualisations using Natural Language \
-            with ChatGPT and Code Llama</h2>", unsafe_allow_html=True)
-
-#st.sidebar.write(":clap: :red[*Code Llama model coming soon....*]")
-st.sidebar.markdown('<a style="text-align: center;padding-top: 0rem;" href="mailto: i.build.apps.4.u@gmail.com">:email:</a> Paula Maddigan and Teo Susnjak', unsafe_allow_html=True)
-st.sidebar.markdown("<h4  style='text-align: center;font-size:small;color:grey;padding-top: 0rem;padding-bottom: .2rem;'>Chat2VIS: Generating Data \
-                    Visualisations via Natural Language using ChatGPT, Codex and GPT-3 \
-                 Large Language Models </h4>", unsafe_allow_html=True)
-
-st.sidebar.caption("(https://doi.org/10.1109/ACCESS.2023.3274199)")
-                 
-st.sidebar.markdown("<h4  style='text-align:center;font-size:small;color:grey;padding-top: 0rem;padding-bottom: .2rem;'>Chat2VIS: Fine-Tuning Data Visualisations using Multilingual \
-Natural Language Text and Pre-Trained Large Language Models </h4>", unsafe_allow_html=True)
-
-st.sidebar.caption("(https://doi.org/10.48550/arXiv.2303.14292)")
-
-st.sidebar.markdown('<a style="text-align: center;padding-top: 0rem;" href="https://blog.streamlit.io/chat2vis-ai-driven-visualisations-with-streamlit-and-natural-language">Blog </a> by Paula Maddigan', unsafe_allow_html=True)
-
-
-available_models = {"ChatGPT-4": "gpt-4","ChatGPT-3.5": "gpt-3.5-turbo","GPT-3": "text-davinci-003",
-                        "GPT-3.5 Instruct": "gpt-3.5-turbo-instruct","Code Llama":"CodeLlama-34b-Instruct-hf"}
-
-
-
-key_col1,key_col2 = st.columns(2)
-openai_key = key_col1.text_input(label = ":key: OpenAI Key:", help="Required for ChatGPT-4, ChatGPT-3.5, GPT-3, GPT-3.5 Instruct.",type="password")
-hf_key = key_col2.text_input(label = ":hugging_face: HuggingFace Key:",help="Required for Code Llama", type="password")
+# Create an empty dictionary for uploaded datasets
+datasets = {}
 
 with st.sidebar:
-    # First we want to choose the dataset, but we will fill it with choices once we've loaded one
-    dataset_container = st.empty()
-
     # Add facility to upload a dataset
     try:
         uploaded_file = st.file_uploader(":computer: Load a CSV file:", type="csv")
-        index_no=0
+        index_no = 0
         if uploaded_file:
             # Read in the data, add it to the list of available datasets. Give it a nice name.
             file_name = uploaded_file.name[:-4].capitalize()
             datasets[file_name] = pd.read_csv(uploaded_file)
-            # We want to default the radio button to the newly added dataset
-            index_no = len(datasets)-1
+            index_no = len(datasets) - 1
     except Exception as e:
         st.error("File failed to load. Please select a valid CSV file.")
         print("File failed to load.\n" + str(e))
-    # Radio buttons for dataset choice
-    chosen_dataset = dataset_container.radio(":bar_chart: Choose your data:",datasets.keys(),index=index_no)#,horizontal=True,)
+
+    if datasets:
+        # Radio buttons for dataset choice
+        chosen_dataset = st.radio(":bar_chart: Choose your data:", list(datasets.keys()), index=index_no)
+
+    # ... [rest of the code remains unchanged] ...
 
     # Check boxes for model choice
     st.write(":brain: Choose your model(s):")
